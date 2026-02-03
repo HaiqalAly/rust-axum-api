940b4b258453b6f7cb8cd546edb0c90cb6bfbc78
HaiqalAly <haiqalaly@gmail.com>
2026-02-03 15:28:07 +0700

    feat: implement healthcheck endpoint using serde

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,19 @@
-use axum::{Router, routing::get};
+use axum::{Json, Router, routing::get};
+use serde::Serialize;
 use tokio::net::TcpListener;
+
+#[derive(Serialize)]
+struct HealthResponse {
+    status: &'static str,
+    version: &'static str,
+}
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     // Build the app route
     let app = Router::new()
-        .route("/", get(root));
+        .route("/", get(root))
+        .route("/health", get(health));
 
     // Run app on port 8080
     let listener = TcpListener::bind("127.0.0.1:8080").await.unwrap();
@@ -17,4 +25,12 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 // Handler that respond with static string
 async fn root() -> &'static str {
     "Hello, World!"
+}
+
+// Handler for health check
+async fn health() -> Json<HealthResponse> {
+    Json(HealthResponse {
+        status: "Server healthy",
+        version: env!("CARGO_PKG_VERSION"),
+    })
 }
