653eb9734ea198302902e3ab2483966404b1dd03
HaiqalAly <haiqalaly@gmail.com>
2026-02-05 17:53:50 +0700

    feat: implement graceful shutdown

---
+++ src/handlers/mod.rs
@@ -0,0 +1,49 @@
+use axum::Json;
+use tracing::info;
+
+use crate::models::HealthResponse;
+use crate::error::AppError;
+
+// Handler that respond with static string
+pub async fn root() -> &'static str {
+    "Hello, World!"
+}
+
+// Handler for health check
+pub async fn health() -> Result<Json<HealthResponse>, AppError> {
+    info!("Everything's fine!");
+    
+    let response = HealthResponse {
+        status: "Server healthy",
+        version: env!("CARGO_PKG_VERSION"),
+    };
+
+    Ok(Json(response))
+}
+
+// Graceful shutdown
+pub async fn shutdown_signal() {
+    let ctrl_c = async {
+        tokio::signal::ctrl_c()
+            .await
+            .expect("failed to install Ctrl+C handler");
+    };
+
+    #[cfg(unix)]
+    let terminate = async {
+        tokio::signal::unix::signal(tokio::signal::unix::SignalKind::terminate())
+            .expect("failed to install signal handler")
+            .recv()
+            .await;
+    };
+
+    #[cfg(not(unix))]
+    let terminate = std::future::pending::<()>();
+
+    tokio::select! {
+        _ = ctrl_c => {},
+        _ = terminate => {},
+    }
+
+    info!("Signal received, starting graceful shutdown...");
+}
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
-use axum::{Router, routing::get};
+use std::time::Duration;
+
+use axum::{Router, routing::get, http::StatusCode};
 use tokio::net::TcpListener;
 use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
-use tower_http::trace::TraceLayer;
+use tower_http::{trace::TraceLayer, timeout::TimeoutLayer};
 
 mod models;
 mod handlers;
@@ -20,18 +22,21 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
                 .into()
             }),
         )
-        .with(tracing_subscriber::fmt::layer())
+        .with(tracing_subscriber::fmt::layer().without_time())
         .init();
 
     // Build the app route
     let app = Router::new()
         .route("/", get(handlers::root))
         .route("/health", get(handlers::health))
-        .layer(TraceLayer::new_for_http());
+        .layer(TraceLayer::new_for_http())
+        .layer(TimeoutLayer::with_status_code(StatusCode::REQUEST_TIMEOUT, Duration::from_secs(10)));
 
     // Run app on port 8080
     let listener = TcpListener::bind("127.0.0.1:8080").await.unwrap();
-    axum::serve(listener, app).await.unwrap();
+    axum::serve(listener, app)
+        .with_graceful_shutdown(handlers::shutdown_signal())
+        .await?;
 
     Ok(())
 }
